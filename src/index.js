b4bde5a268c8fb3ab0a1347948e63874db107c33
ykruk <yulka.kr@gmail.com>
2019-10-07 23:29:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function zeros(expression) {
-  // your solution
+  function multiply(str) {
+		let numbers = str.split('*');
+		let output = BigInt(1);
+		
+		numbers.forEach(function(elem, i, array){
+			let multNumber;
+			let resNumber = BigInt(1);
+			if(elem.match(/!/g).length === 2) {
+				multNumber = elem.slice(0, -2);
+				for(let i = multNumber; i > 0; i -= 2) {
+					resNumber *= BigInt(i);
+				}
+			} else if(elem.match(/!/g).length === 1) {
+				multNumber = elem.slice(0, -1);
+				for(let i = multNumber; i > 0; i--) {
+					resNumber *= BigInt(i);
+				}
+			};
+			output *= resNumber;
+		})
+		
+		return output;
+	}
+
+  let multString = multiply(expression).toString(10);
+  let counter = 0;
+  
+  for(let i = multString.length; i > 0; i--) {
+    multString = multString.slice(0, i);
+    multString.slice(-1) === '0' ? counter++ : i = 0;
+  }
+  
+  return counter;
+	
 }
